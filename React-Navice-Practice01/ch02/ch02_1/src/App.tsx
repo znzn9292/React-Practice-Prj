f22f4794e2e5d889e4c5ba3eeb567424b33e8e9b
jeongseonhwan <znzn9292@gmail.com>
2021-07-16 13:54:12 +0900

    Delete: React-Native-Prj

--- React-Navice-Practice01/ch02/ch02_1/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Text } from 'react-native'
-
-export default function App() {
-  // console.log('App called')
-  const textElement = React.createElement(Text, null, 'Hello world!')
-  return textElement
-}
