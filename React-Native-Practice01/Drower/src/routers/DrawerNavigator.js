ffc8bb74522f1b8280649899b9458f4930d657e6
znzn <znzn9292@gmail.com>
2021-07-22 03:05:06 +0900

    Fix: DrawerNavigator.js

--- React-Native-Practice01/Drower/src/routers/DrawerNavigator.js
+++ React-Native-Practice01/Drower/src/routers/DrawerNavigator.js
@@ -10,20 +10,15 @@ const HomeStack = createStackNavigator();
 const ImageStack = createStackNavigator();
 const AboutStack = createStackNavigator();
 
-// const TabBottomStack = createBottomTabNavigator();
-
 const HomeStackRouter = () => {
-    // return <HomeStack.Navigator>
-    //     <HomeStack.screen name="home" component={HomeStack} />
-    // </HomeStack.Navigator>
+    return <HomeStack.Navigator>
+        <HomeStack.Screen name="home" component={HomeScreen} />
+    </HomeStack.Navigator>
 }
 
 const DrawerNavigator = () => {
     return <Drawer.Navigator>
-        {/* <Drawer.Screen name="homestack" component={HomeStackRouter} />
-        <Drawer.Screen name="imagelist" component={ImageListScreen} />
-        <Drawer.Screen name="about" component={AboutScreen} /> */}
-        <Drawer.Screen name="homestack" component={HomeScreen} />
+        <Drawer.Screen name="homestack" component={HomeStackRouter} />
         <Drawer.Screen name="imagelist" component={ImageListScreen} />
         <Drawer.Screen name="about" component={AboutScreen} />
     </Drawer.Navigator>
