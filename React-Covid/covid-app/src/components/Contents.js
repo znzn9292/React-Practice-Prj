8b00544b9d3083e9fecb0f9a239f083a4c545182
znzn <znzn9292@gmail.com>
2021-08-03 23:53:54 +0900

    Modify: covid-app

--- React-Covid/covid-app/src/components/Contents.js
+++ React-Covid/covid-app/src/components/Contents.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { Bar, Doughnut, Line } from 'react-chartjs-2'
+// import { Bar, Doughnut, Line } from 'react-chartjs-2'
+
 const Contents = () => {
 
     useEffect(() => {
         const fetchEvents = async () => {
             const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
-            console.log(res)
         }
         fetchEvents()
     })
@@ -15,7 +15,23 @@ const Contents = () => {
         <section>
             <h2>국내 코로나 현황</h2>
             <div className="contents">
-
+                <div>
+                    {/* <Bar data={confirmedData} options={
+                        {
+                            title: {
+                                display: true,
+                                text: "누적 확인자 추이",
+                                fontSize: 16
+                            }
+                        },
+                        {
+                            legend: {
+                                display: true,
+                                position: "bottom"
+                            }
+                        }
+                    } /> */}
+                </div>
             </div>
         </section>
     )
